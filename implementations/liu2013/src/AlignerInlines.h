c083e98f4cc2b762355d481052d3aff6d300c7cf
Richard Barnes <rbarnes@umn.edu>
2018-07-30 15:44:10 -0700

    More string literal issues resolved

--- implementations/liu2013/src/AlignerInlines.h
+++ implementations/liu2013/src/AlignerInlines.h
@@ -582,16 +582,16 @@
 		 "        xorq      r11, r11            \n"
 
 #define ONESTEP16(H, N, F, V)	         		    \
-                 "        paddsw    "V"(rax), "H"       \n" \
-                 "        pmaxsw    "F", "H"            \n" \
-                 "        pmaxsw    xmm12, "H"          \n" \
-                 "        pmaxsw    "H", xmm13          \n" \
-		 "        psubsw    xmm15, "F"          \n" \
+                 "        paddsw    " V "(rax), " H "       \n" \
+                 "        pmaxsw    " F ", " H "            \n" \
+                 "        pmaxsw    xmm12, " H "          \n" \
+                 "        pmaxsw    " H ", xmm13          \n" \
+		 "        psubsw    xmm15, " F "          \n" \
 		 "        psubsw    xmm15, xmm12        \n" \
-		 "        movdqa    "H", "N"            \n" \
-		 "        psubsw    xmm14, "H"          \n" \
-		 "        pmaxsw    "H", xmm12          \n" \
-		 "        pmaxsw    "H", "F"            \n" \
+		 "        movdqa    " H ", " N "            \n" \
+		 "        psubsw    xmm14, " H "          \n" \
+		 "        pmaxsw    " H ", xmm12          \n" \
+		 "        pmaxsw    " H ", " F "            \n" \
 
 	inline void donormal16(__m128i * Sm, /* r9  */
 	__m128i * hep, /* rdi */
