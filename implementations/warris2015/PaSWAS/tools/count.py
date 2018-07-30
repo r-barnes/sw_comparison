4440a22cc6cb37c65ee83eae6cd04f9f594e14fd
Richard Barnes <rbarnes@umn.edu>
2018-07-30 16:44:20 -0700

    Added command-line argument checking to count.py

--- implementations/warris2015/PaSWAS/tools/count.py
+++ implementations/warris2015/PaSWAS/tools/count.py
@@ -6,14 +6,17 @@ from Bio.Seq import Seq
 
 mem_size_matrix = 32*8
 mem_fill_factor = 0.9
-mem_size_gpu = int(sys.argv[3])*1024*1024
+mem_size_gpu    = int(sys.argv[3])*1024*1024
 
+if len(sys.argv)!=4:
+  print("{0} [sequences.fasta] [targets.fasta] [mem on gpu]".format(sys.argv[0]))
+  sys.exit(-1)
 
 seqList1 = list(SeqIO.parse(open(sys.argv[1],"r"), "fasta"))
 seqList2 = list(SeqIO.parse(open(sys.argv[2],"r"), "fasta"))
 
 length_sequences = int(math.ceil((len(seqList1[0])/8.0)*8.0))
-length_targets = int(math.ceil((len(seqList2[0])/8.0)*8.0))
+length_targets   = int(math.ceil((len(seqList2[0])/8.0)*8.0))
                                
 number_targets = int(math.floor(math.sqrt((mem_size_gpu * mem_fill_factor) / ((length_targets * length_targets * (mem_size_matrix) +
                             (length_targets * length_targets * 32) /
