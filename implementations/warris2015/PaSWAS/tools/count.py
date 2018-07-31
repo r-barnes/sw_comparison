b4e7d118fb4d931be3be15772bc14ebd722a41e3
Richard Barnes <rbarnes@umn.edu>
2018-07-30 17:40:06 -0700

    Fixing math in Warris2015 count

--- implementations/warris2015/PaSWAS/tools/count.py
+++ implementations/warris2015/PaSWAS/tools/count.py
@@ -5,7 +5,7 @@ from Bio import  SeqIO
 from Bio.Seq import Seq
 
 if len(sys.argv)!=4:
-  print("{0} [sequences.fasta] [targets.fasta] [mem on gpu]".format(sys.argv[0]))
+  print("{0} [sequences.fasta] [targets.fasta] [mem on gpu in MB]".format(sys.argv[0]))
   sys.exit(-1)
 
 mem_size_matrix = 32*8
@@ -15,8 +15,11 @@ mem_size_gpu    = int(sys.argv[3])*1024*1024
 seqList1 = list(SeqIO.parse(open(sys.argv[1],"r"), "fasta"))
 seqList2 = list(SeqIO.parse(open(sys.argv[2],"r"), "fasta"))
 
-length_sequences = int(math.ceil((len(seqList1[0])/8.0)*8.0))
-length_targets   = int(math.ceil((len(seqList2[0])/8.0)*8.0))
+max_seq_len    = max([len(x) for x in seqList1])
+max_target_len = max([len(x) for x in seqList2])
+
+length_sequences = int(math.ceil((max_seq_len/8.0)*8.0))
+length_targets   = int(math.ceil((max_target_len/8.0)*8.0))
                                
 number_targets = int(math.floor(math.sqrt((mem_size_gpu * mem_fill_factor) / ((length_targets * length_targets * (mem_size_matrix) +
                             (length_targets * length_targets * 32) /
