a3862d8934db3943f062415c6bcaeea64bc39ff3
Richard Barnes <rbarnes@umn.edu>
2020-03-07 18:40:52 -0700

    Fix inlining issue in liu2014b_cushaw2_gpu

--- implementations/liu2014b_cushaw2_gpu/bamreader/bgzf.c
+++ implementations/liu2014b_cushaw2_gpu/bamreader/bgzf.c
@@ -72,18 +72,15 @@ static const int BGZF_XLEN = 6; // BGZF_LEN+4
 static const int GZIP_WINDOW_BITS = -15; // no zlib header
 static const int Z_DEFAULT_MEM_LEVEL = 8;
 
-inline
 void packInt16(uint8_t* buffer, uint16_t value) {
 	buffer[0] = value;
 	buffer[1] = value >> 8;
 }
 
-inline
 int unpackInt16(const uint8_t* buffer) {
 	return (buffer[0] | (buffer[1] << 8));
 }
 
-inline
 void packInt32(uint8_t* buffer, uint32_t value) {
 	buffer[0] = value;
 	buffer[1] = value >> 8;
